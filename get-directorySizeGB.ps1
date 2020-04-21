5b075a14ea5351a92dd5c5fa026363981b62ab78
Michael M <m.migsch@gmail.com>
2020-04-21 19:00:09 +0200

    Update get-directorySizeGB.ps1

--- get-directorySizeGB.ps1
+++ get-directorySizeGB.ps1
@@ -1,4 +1,11 @@
-function get-dirInfo($dir) {
+# Command Line Parameters
+param([string]$dir)
+
+# Functions Section
+function Get-DirectorySizeGB($dir) {
     $result = Get-ChildItem $dir -Recurse | Measure-Object -Property Length -Sum
     return [math]::Round(($result).sum/1GB, 2)
 }
+
+# Main Processing
+Get-DirectorySizeGB $dir
