0d6906e0879d72e9e1decefeafd28412c310c16f
Michael M <m.migsch@gmail.com>
2020-10-25 20:47:59 +0100

    Create New-Password.ps1

---
+++ New-Password.ps1
@@ -0,0 +1,17 @@
+# Autor: Michael Migsch
+# Beschreibung: Erstellt ein Passwort variabler Länge.
+# Version: 1.0
+
+# Funktion Passwortgenerator
+function New-Password ([int] $Anzahl) {
+    $Kennwort = ""
+    $PasswortGenerator = New-Object System.Random
+
+    for ($i=0;$i -lt $Anzahl;$i++) {
+        $Kennwort = $Kennwort + [char]$PasswortGenerator.Next(33,127)
+    }
+    
+    $Kennwort | clip.exe
+
+    return $Kennwort
+}
