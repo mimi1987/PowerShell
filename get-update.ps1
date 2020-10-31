dce01eeb25118819fbe087e638d79656a0800c80
Michael M <m.migsch@gmail.com>
2020-10-31 14:21:26 +0100

    Create get-update.ps1

---
+++ get-update.ps1
@@ -0,0 +1,15 @@
+# Author: Michael Migshc
+# Description: Gets all of the uninstalled updates of type software
+# Parameters: Just the Computername, which uses $env:computername as default value.
+
+# Functions
+
+function Get-Update([string]$Computer=$env:COMPUTERNAME) {
+    [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.VisualBasic")
+    $session = [Microsoft.VisualBasic.Interaction]::CreateObject("Microsoft.Update.Session", $Computer)
+    $searcher = $session.CreateUpdateSearcher()
+    $searcher.Search("IsInstalled=0 and Type='Software' and IsHidden=0")
+}
+
+$updateinfo = Get-Update
+$updateinfo.updates | Format-Table title, LastDeploymentChangeTime
