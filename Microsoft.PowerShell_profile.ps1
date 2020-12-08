1ce82b71651b85557540cdd5188d5e3b7c2456b2
Michael M <m.migsch@gmail.com>
2020-12-08 22:05:26 +0100

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+$Tag = (Get-Date).DayOfYear
+$Jahr = (Get-Date).Year
+
+Write-Host "Hallo, $env:USERNAME!" -ForegroundColor White -BackgroundColor Blue
+Write-Host "Heute ist der $Tag. Tag des Jahres $Jahr."
+# ...und viele weitere Definitionen
+
+# Sets the apperance of the console
+
+Set-PSReadLineOption -Colors @{
+  "Number" = "DarkMagenta"
+  "Parameter" = "Cyan"
+  "Variable" = "#FFA400"
+  "Keyword" = "DarkRed"
+}
+
+
+# Sets the prompt
+
+function prompt
+{
+  "PS $(Get-Date -Format HH:mm)>"
+  $Host.UI.RawUI.WindowTitle = "$(Get-Location) +++ User: $env:USERNAME"
+}
