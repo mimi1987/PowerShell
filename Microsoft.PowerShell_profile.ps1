36e901a3f0abce1c2c6776a96b1db59fd214655f
Michael M <m.migsch@gmail.com>
2020-12-08 22:06:14 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
-$Tag = (Get-Date).DayOfYear
-$Jahr = (Get-Date).Year
+$Day = (Get-Date).DayOfYear
+$Year = (Get-Date).Year
 
 Write-Host "Hallo, $env:USERNAME!" -ForegroundColor White -BackgroundColor Blue
-Write-Host "Heute ist der $Tag. Tag des Jahres $Jahr."
+Write-Host "Heute ist der $Day. Tag des Jahres $Year."
 # ...und viele weitere Definitionen
 
 # Sets the apperance of the console
